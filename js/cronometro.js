294b322ffde8264f34c9c461dbae32ea4a0cdcd1
Ana <uo300568@uniovi.es>
2025-11-09 21:43:41 +0100

    Session7

---
+++ js/cronometro.js
@@ -0,0 +1,76 @@
+class Cronometro {
+    constructor(){
+        this.tiempo = 0;
+        this.inicio = null;
+        this.corriendo = null;
+        this.enMarcha = false;
+    }
+
+    arrancar(){
+        try{
+            if(typeof Temporal === 'undefined'){
+                throw new Error("Temporal no disponible");
+            }
+            this.inicio = Temporal.Now.instant();
+
+        }catch(error){
+            this.inicio = new Date();
+        }
+
+        this.corriendo = setInterval(this.actualizar.bind(this), 100);
+        this.enMarcha = true;
+    }
+
+    actualizar(){
+        try{
+            if(typeof Temporal === 'undefined'){
+                throw new Error("Temporal no disponible");
+            }
+            const ahora = Temporal.Now.instant();
+            this.tiempo = ahora.epochMilliseconds - this.inicio.epochMilliseconds;
+        }catch(error){
+            const ahora = new Date();
+            this.tiempo = ahora.getTime() - this.inicio.getTime();
+        }
+        this.mostrar();
+    }
+
+    parar() {
+        if (!this.enMarcha) return;
+
+        clearInterval(this.corriendo);
+        this.enMarcha = false;
+    }
+
+     reiniciar() {
+        clearInterval(this.corriendo);
+        this.tiempo = 0;
+        this.inicio = null;
+        this.enMarcha = false;
+        this.mostrar();
+    }
+
+     mostrar() {
+        const minutos = parseInt(this.tiempo / 60000);
+        const segundos = parseInt((this.tiempo % 60000) / 1000);
+        const decimas = parseInt((this.tiempo % 1000) / 100);
+
+        const formato =
+        String(minutos).padStart(2, "0") +
+        ":" +
+        String(segundos).padStart(2, "0") +
+        "." +
+        String(decimas);
+
+        const parrafo = document.querySelector("main p");
+
+        if (parrafo) {
+            parrafo.textContent = formato;
+        } else {
+            console.warn("No se encontró ningún <p> dentro de <main>");
+        }
+    }
+
+}
+
+const c = new Cronometro();
--- js/memoria.js
+++ js/memoria.js
@@ -1,11 +1,104 @@
-class Memoria {
-    constructor(){}
+"use strict";
 
-    voltearCarta(carta){
-        carta.dataset.estado = "voletada"
+class Memoria {
+    constructor(){
+        this.tablero_bloqueado = true;
+        this.primera_carta = null;
+        this.segunda_carta = null;
+
+        this.barajarCartas();
+        
+        this.tablero_bloqueado = false;
+
+        this.cronometro = new Cronometro();
+        this.cronometro.arrancar();
     }
 
-    
+    voltearCarta(carta){
+        if (this.tablero_bloqueado) return;
+
+        const estado = carta.dataset.estado;
+        if (estado === "volteada" || estado === "revelada") return;
+        
+        carta.dataset.estado = "volteada";
+
+        if (!this.primera_carta) {
+            this.primera_carta = carta;
+        } else {
+            this.segunda_carta = carta;
+            this.comprobarPareja();
+        }
+    }
+
+    barajarCartas() {
+        const tablero = document.getElementById("tablero");
+        const cartas = Array.from(tablero.querySelectorAll(".carta"));        
+
+        for (let i = cartas.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [cartas[i], cartas[j]] = [cartas[j], cartas[i]];
+        }
+
+        for (let carta of cartas) {
+            tablero.appendChild(carta);
+        }
+    }
+
+    reiniciarAtributos() {
+        this.tablero_bloqueado = false;
+        this.primera_carta = null;
+        this.segunda_carta = null;
+    }
+
+    deshabilitarCartas(){
+        this.primera_carta.dataset.estado = "revelada";
+        this.segunda_carta.dataset.estado = "revelada";
+
+        this.primera_carta.onclick = null;
+        this.segunda_carta.onclick = null;
+
+        this.comprobarJuego();
+        this.reiniciarAtributos();
+    }
+
+    comprobarJuego() {
+        const cartas = document.querySelectorAll(".carta");
+        let todasReveladas = true;
+
+        for (let i = cartas.length - 1; i > 0; i--) {
+            if (cartas[i].dataset.estado !== "revelada") {
+                todasReveladas = false;
+                break;
+            }
+        }
+
+        if (todasReveladas) {
+            this.cronometro.parar();
+            setTimeout(() => {
+            alert("¡Enhorabuena! Has encontrado todas las parejas");
+            }, 300); 
+        }
+    }
+
+    cubrirCartas(){
+        this.tablero_bloqueado = true;
+        setTimeout(() => {
+            this.primera_carta.dataset.estado = null;
+            this.segunda_carta.dataset.estado = null;
+            this.reiniciarAtributos();
+        }, 1000);
+    }
+
+    comprobarPareja(){
+        const img1 = this.primera_carta.children[1];
+        const src1 = img1.getAttribute("src");
+
+        const img2 = this.segunda_carta.children[1];
+        const src2 = img2.getAttribute("src");
+
+        src1 == src2 ? this.deshabilitarCartas() : this.cubrirCartas();
+    }
+
 }
 
 const juego = new Memoria();
